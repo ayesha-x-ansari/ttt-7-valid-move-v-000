d31edc2ed61d1f213e79ea94a37835d13ad894b2
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-12-12 16:27:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,7 +5,7 @@ board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 def valid_move?(board, index)
   if position_taken?(board, index) &&  index.between?(0, 8)
         puts  "valid move"
-        return truthiness
+        return true
   else
         return false
   end
