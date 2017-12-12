0ca1e75060b8ae7e621d9ac4a19a42d107214386
ayesha ansari <ayesha.x.ansari@gmail.com>
2017-12-12 15:35:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def valid_move?(board, index)
-  if position_taken?
+  if position_taken(board,index)?
         return true
   else
         return false
